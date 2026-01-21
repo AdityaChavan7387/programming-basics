b487da51078c30d99b51c1a3ba8ef529b7dc0ea1
Aditya Chavan <adityachavan7387@gmail.com>
2026-01-21 21:28:58 +0530

    Added another new program releted to pointers in c

---
+++ C Programming/Pointers/pointer3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int x;
+    int *ptr;
+
+    ptr = &x; // Assigning the address of x to pointer ptr
+    *ptr += 4; // Assigning value 4 to the location pointed by ptr
+
+    printf("Value of x: %d\n", x); // Output the value of x
+    printf("Value via pointer ptr: %d\n", *ptr); // Output the value via pointer
+
+
+    return 0;
+}
