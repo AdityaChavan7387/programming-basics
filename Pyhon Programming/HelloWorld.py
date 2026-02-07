2ab734220e22959d35e80557582cbbd657a46a57
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-07 16:26:15 +0530

    Added new program in python folder related to variables

--- Pyhon Programming/HelloWorld.py
+++ Pyhon Programming/HelloWorld.py
@@ -1,2 +1,11 @@
 print("Hello, World!")
 # This is a simple Python program that prints "Hello, World!" to the console.
+
+print("This is The Fool", "A Tarot Card")
+# This line prints "This is The Fool A Tarot Card" to the console.
+
+print(23)
+# This line prints the number 23 to the console.
+
+print(23 + 2)
+# This line prints the result of the addition 23 + 2, which is 25, to the console.
---
+++ Pyhon Programming/variables_in_python.py
@@ -0,0 +1,5 @@
+name = "The Fool" # This variable stores the name of the Tarot card, which is "The Fool".
+Age = "10000+" # This variable stores the age of The Fool, which is "10000+".
+pathway = "Seer Pathway" # This variable stores the pathway associated with The Fool, which is "Seer Pathway".
+
+
