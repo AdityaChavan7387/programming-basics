c181bc94370e4e3b3c6ccc4081cb20428249a93f
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-07 16:49:57 +0530

    Added new program in python folder related to variablestypes

---
+++ Pyhon Programming/varables_in_python.py
@@ -0,0 +1,12 @@
+name = "The Fool" # This variable stores the name of the Tarot card, which is "The Fool".
+Age = "10000+" # This variable stores the age of The Fool, which is "10000+".
+pathway = "Seer Pathway" # This variable stores the pathway associated with The Fool, which is "Seer Pathway".
+
+print("My Name is : ", name)
+# This line prints the value of the variable 'name', which is "The Fool", to the console.
+
+print("My Age is : ", Age)
+# This line prints the value of the variable 'Age', which is "10000+", to the console.
+
+print("My Power Pathway is : ", pathway)
+# This line prints the value of the variable 'pathway', which is "Seer Pathway", to the console.
---
+++ Pyhon Programming/variable_types_in_python.py
@@ -0,0 +1,10 @@
+name = "The Fool" # This variable stores the name of the Tarot card, which is "The Fool".
+Age = 10000 # This variable stores the age of The Fool, which is 10000.
+pathwaysquence = 0.1 # This variable stores the pathway sequence number associated with The Fool, which is 0.111.
+
+print(type(name))
+# This line prints the data type of the variable 'name', which is <class 'str'>, indicating that it is a string.
+print(type(Age))
+# This line prints the data type of the variable 'Age', which is <class 'int'>, indicating that it is an integer.
+print(type(pathwaysquence))
+# This line prints the data type of the variable 'pathwaysquence', which is <class 'float'>, indicating that it is a floating-point number.
