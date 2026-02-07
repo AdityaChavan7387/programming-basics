1f2866b713a3aa55d0a2207726245293c2a84515
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-07 17:02:51 +0530

    Added new program in python folder related to DataTypes

---
+++ Pyhon Programming/Data_types_in_python.py
@@ -0,0 +1,9 @@
+age = 22 # integer
+old = True # boolean
+a = None # NoneType
+b = 3.14 # float
+
+print(type(age)) # integer type will be printed
+print(type(old)) # boolean type will be printed
+print(type(a)) # NoneType will be printed
+print(type(b)) # float type will be printed
--- Pyhon Programming/varables_in_python.py
+++ Pyhon Programming/varables_in_python.py
@@ -1,5 +1,5 @@
 name = "The Fool" # This variable stores the name of the Tarot card, which is "The Fool".
-Age = "10000+" # This variable stores the age of The Fool, which is "10000+".
+Age = 10000 # The variable 'Age' is assigned the value 10000.
 pathway = "Seer Pathway" # This variable stores the pathway associated with The Fool, which is "Seer Pathway".
 
 print("My Name is : ", name)
