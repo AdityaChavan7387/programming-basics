a2f8f6947943f9493126a1b0b452618b176be832
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-07 16:29:31 +0530

    Added new program in python folder related to variables and added dicriptions

--- Pyhon Programming/variables_in_python.py
+++ Pyhon Programming/variables_in_python.py
@@ -1,5 +1,14 @@
-name = "The Fool" # This variable stores the name of the Tarot card, which is "The Fool".
-Age = "10000+" # This variable stores the age of The Fool, which is "10000+".
-pathway = "Seer Pathway" # This variable stores the pathway associated with The Fool, which is "Seer Pathway".
+name = "Name - The Fool" # This variable stores the name of the Tarot card, which is "The Fool".
+Age = "Age - 10000+" # This variable stores the age of The Fool, which is "10000+".
+pathway = "Pathway - Seer Pathway" # This variable stores the pathway associated with The Fool, which is "Seer Pathway".
+
+print(name)
+# This line prints the value of the variable 'name', which is "The Fool", to the console.
+
+print(Age)
+# This line prints the value of the variable 'Age', which is "10000+", to the console.
+
+print(pathway)
+# This line prints the value of the variable 'pathway', which is "Seer Pathway", to the console.
 
 
