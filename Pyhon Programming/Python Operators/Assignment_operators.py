e7da91a9cf9c03d5eeee4dc3cfc87293c11a7d7f
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-08 14:00:12 +0530

    Added New folder for operators in python and added Assignment_operators program

---
+++ Pyhon Programming/Python Operators/Assignment_operators.py
@@ -0,0 +1,25 @@
+# Assignment Operators in Python
+
+num = 10
+num1 = 10
+num2 = 10
+num3 = 10
+num4 = 10
+num5 = 10   
+num6 = 10
+
+num += 10 # num is now 30
+num1 -= 5 # num is now 25
+num2 *= 2 # num is now 50
+num3 /= 5 # num is now 10.0
+num4 //= 3 # num is now 3.0
+num5 %= 2 # num is now 1.0
+num6 **= 3 # num is now 1.0
+
+print("Total num value : ", num) 
+print("Total num1 value : ", num1) 
+print("Total num2 value : ", num2) 
+print("Total num3 value : ", num3) 
+print("Total num4 value : ", num4) 
+print("Total num5 value : ", num5) 
+print("Total num6 value : ", num6) 
