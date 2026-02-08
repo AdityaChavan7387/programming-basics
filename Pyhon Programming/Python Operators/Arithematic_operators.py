c04b84dc2c1b3e01288b80fa786b32bf67652361
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-08 13:59:12 +0530

    Added New folder for operators in python and added arethematic program

---
+++ Pyhon Programming/Python Operators/Arithematic_operators.py
@@ -0,0 +1,13 @@
+# Arithematic Operators in Python
+
+A = 10 
+
+B = 2
+
+print(A + B) # Addition
+print(A - B) # Subtraction
+print(A * B) # Multiplication
+print(A / B) # Division
+print(A % B) # Modulus
+print(A ** B) # Exponentiation
+print(A // B) # Floor Division
