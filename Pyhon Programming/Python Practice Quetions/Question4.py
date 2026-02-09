b327b9b23924e7896cdc1d6a646746e48a8f88c8
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-09 13:35:42 +0530

    Added New folder for Python Practice Quetions and added Question3 in it

---
+++ Pyhon Programming/Python Practice Quetions/Question4.py
@@ -0,0 +1,5 @@
+# Write A Program to input 2 int numbers , a and b. print true if a is greater than or equal to b. if not print false.
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(a >= b)
