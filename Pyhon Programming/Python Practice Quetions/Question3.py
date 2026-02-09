0d79c316f921643a57786eba4c436675bb56aba9
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-09 13:35:12 +0530

    Added New folder for Python Practice Quetions and added Question3 in it

---
+++ Pyhon Programming/Python Practice Quetions/Question3.py
@@ -0,0 +1,8 @@
+
+# write a program to input 2 floating point numbers and print their average.
+
+a = float(input("Enter the first floating point number: "))
+b = float(input("Enter the second floating point number: "))
+
+average = (a + b) / 2
+print("The Average of the two numbers is: ", average)
