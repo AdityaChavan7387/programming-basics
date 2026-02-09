1b0d9e77e9efde564bbd25e9bcffb479d1ca2d22
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-09 13:34:18 +0530

    Added New folder for Python Practice Quetions and added Question1 in it

---
+++ Pyhon Programming/Python Practice Quetions/Question1.py
@@ -0,0 +1,12 @@
+
+# Write A Program to input 2 numbers from user and print their sum.
+
+# Taking input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculating the sum
+sum_result = num1 + num2
+
+# Printing the sum1
+print("The Total Sum is:", sum_result)
