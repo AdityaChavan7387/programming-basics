f79e6481f6b08d1a032c8591c669cc4b303bf620
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-10 23:30:00 +0530

    Added New program in python that has all the basic string functions present

---
+++ Pyhon Programming/Strings & Conditional Satements/String_functions.py
@@ -0,0 +1,17 @@
+
+# In This we will look into some of the string functions in python
+
+str = "Hello World"
+
+str.endswith("World") # This will return true as the string ends with "World"
+
+str.startswith("Hello") # This will return true as the string starts with "Hello"
+
+str.find("o") # This will return the index of the first occurrence of "o" in the string, which is 4
+
+str.count("o") # This will return the number of occurrences of "o" in the string, which is 2    
+
+str.capitalize() # This will return the string with the first letter capitalized, which is "Hello world"
+
+str.replace("o", "a") # This will return the string with all occurrences of "o" replaced with "a", which is "Hella Warld"
+
