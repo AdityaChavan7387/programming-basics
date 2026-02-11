efb879320234204c45d6109ce6320a820ec112d9
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-11 13:58:59 +0530

    Added New practice question program no.5 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question5.py
@@ -0,0 +1,7 @@
+
+# Write A Program To Input User's First Name & Print It's Length.
+
+first_name = input("Enter your first name: ")
+
+string_length = len(first_name)
+print("The length of your first name is:", string_length)
