f692dae05f8ba721ff7f6b933f947db2f098ec05
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-11 14:04:06 +0530

    Added New practice question program no.6 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question6.py
@@ -0,0 +1,8 @@
+
+# Write A Program To Find the occurrence of '$' in a String.
+
+input_string = input("Enter a string: ")
+
+occurrence_count = input_string.count('$')
+print("The occurrence of '$' in the string is:", occurrence_count)
+
