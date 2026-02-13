754f5e53d613db009d452628da8689f5f2143808
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-13 19:32:09 +0530

    Added New practice program Question12 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question12.py
@@ -0,0 +1,14 @@
+# Write A Program to find the greatest of Four numbers entered by user.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+num4 = float(input("Enter fourth number: "))
+
+if (num1 >= num2) and (num1 >=num3) and (num1 >= num4):
+    print("The Greatest Number is From num1: ", num1)
+elif (num2 >= num1) and (num2 >= num3) and (num2 >= num4):
+    print("The Greatest Number is From num2: ", num2)
+elif (num3 >= num1) and (num3 >= num2) and (num3 >= num4):
+    print("The Greatest Number is From num3: ", num3)
+else:    print("The Greatest Number is From num4: ", num4)
