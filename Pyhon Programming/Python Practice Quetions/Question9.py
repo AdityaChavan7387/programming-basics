7093b3c35792a473c67b22d627a4c5f3d1325e98
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-13 18:57:48 +0530

    Added New practice program Question9 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question9.py
@@ -0,0 +1,9 @@
+
+# Write A Program To Check if a number entered by the user is odd or even.
+
+Num = int(input("Enter a number: "))
+
+if Num % 2 == 0:
+    print(f"{Num} is an even number.")
+else:
+    print(f"{Num} is an odd number.")
