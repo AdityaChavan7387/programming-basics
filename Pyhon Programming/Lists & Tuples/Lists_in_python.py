a2c69dfe8933233d52205bd4694af03301855403
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-15 22:15:04 +0530

    Added New folder related to lists and tuples in python and a simple lists intro. program

---
+++ Pyhon Programming/Lists & Tuples/Lists_in_python.py
@@ -0,0 +1,11 @@
+
+marks = [90, 80, 70, 60, 50]
+
+print(marks[0])  # Output: 90 This will print the first element of the list, which is 90.
+print(marks[1])  # Output: 80 This will print the second element of the list, which is 80. and so on ....
+
+print(marks) # This Will print the entire list of marks, which is [90, 80, 70, 60, 50].
+
+print(type(marks))  # Output: <class 'list'> This will print the type of the variable marks, which is a list.
+
+print(len(marks))  # Output: 5 This will print the length of the list, which is 5.
