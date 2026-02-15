3836e097e20f56ff30a1b7c4a3e9b4fdfa3c6271
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-15 22:22:30 +0530

    Added New prgram related to student lists in python

---
+++ Pyhon Programming/Lists & Tuples/student_lists.py
@@ -0,0 +1,8 @@
+# Creating A Student lists with Name, Marks, Age, And Grade
+student1 = ["John Doe", 85, 20, "A"]
+print(student1) # Output: ['John Doe', 85, 20, 'A']
+
+print(student1[0]) # Output: John Doe
+
+student1[0] = "Sam Smith" # Changing the name of the student
+print(student1) # Output: ['Sam Smith', 85, 20, 'A'] with updated name
