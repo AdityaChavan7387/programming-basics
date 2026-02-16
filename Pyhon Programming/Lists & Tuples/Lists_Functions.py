d23e797e8f54a3ef3f7f1a377625450c43b8d0ca
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-16 21:42:47 +0530

    Added New prgram related to List Functions in python

---
+++ Pyhon Programming/Lists & Tuples/Lists_Functions.py
@@ -0,0 +1,28 @@
+# Here are some functions that can be used with lists in Python:
+
+list = [2, 1, 3]
+list.append(4) # Adds an element to the end of the list
+print(list)
+
+list.sort() # Sorts the list in ascending order
+print(list)
+
+list.sort(reverse=True) # Sorts the list in descending order
+print(list)
+
+list.reverse() # Reverses the order of the list
+print(list)
+
+list.insert(1, 5) # Inserts an element at a specific index (index 1 in this case)
+print(list)
+
+list.remove(3) # Removes the first occurrence of the specified value (3 in this case)
+print(list)
+
+list.pop(2) # Removes and returns the element at the specified index (index 2 in this case)   
+print(list)
+
+# This Also Applies To characters in a list for example fruits = ['banana', 'apple', 'cherry']
+fruits = ['banana', 'apple', 'cherry']
+fruits.sort() # Sorts the list in alphabetical order
+print(fruits) 
