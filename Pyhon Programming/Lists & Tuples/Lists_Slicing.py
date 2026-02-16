b93f41f6150e1af9fd07e2087e83fe4db53124fa
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-16 21:24:53 +0530

    Added New prgram related to Lists Slicing in python with minor alteration

--- Pyhon Programming/Lists & Tuples/Lists_Slicing.py
+++ Pyhon Programming/Lists & Tuples/Lists_Slicing.py
@@ -7,3 +7,6 @@ print(marks[0:3])
 print(marks[:3])  # This is equivalent to marks[0:3]
 print(marks[1:])  # This will give us all marks starting from index 1 to the end of the list
 
+# now for negative indexing
+print(marks[-3:])  # This will give us the last three marks
+print(marks[:-2])  # This will give us all marks except the last two
