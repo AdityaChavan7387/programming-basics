e19ec3b9025e3d90582e32c445be7ba5453cc764
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-16 21:23:28 +0530

    Added New prgram related to Lists Slicing in python

---
+++ Pyhon Programming/Lists & Tuples/Lists_Slicing.py
@@ -0,0 +1,9 @@
+
+marks = [90, 80, 70, 60, 50]
+
+# Slicing the list to get the first three marks
+print(marks[0:3])
+
+print(marks[:3])  # This is equivalent to marks[0:3]
+print(marks[1:])  # This will give us all marks starting from index 1 to the end of the list
+
