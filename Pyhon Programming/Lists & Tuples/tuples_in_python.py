c1459ff0673839ae25db45d0cc5e3eed9ab4215d
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-16 21:56:31 +0530

    Added New prgram related to tuples in python

---
+++ Pyhon Programming/Lists & Tuples/tuples_in_python.py
@@ -0,0 +1,7 @@
+tup = (20, 30, 40, 50) # creating a tuple. The tuples are immutable, and are used in () brackets. The tuples can contain any type of data, and can be nested.
+print(tup)
+print(type(tup))
+# Accessing the elements of a tuple
+print(tup[0]) # accessing the first element of the tuple
+print(tup[1]) # accessing the second element of the tuple
+
