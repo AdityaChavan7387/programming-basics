c2d509d6c2275c28fdc936f557daa1d01b52e904
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-16 22:04:24 +0530

    Added New prgram related to tuples and its functions in python

---
+++ Pyhon Programming/Lists & Tuples/Tuple_Functions.py
@@ -0,0 +1,6 @@
+tup = (2, 1, 3, 4, 5, 1, 1)
+
+print(tup.index(3))  # returns the index of the first occurrence of the value 3
+
+print(tup.count(1))  # returns the number of times the value 3 appears in the tuple
+
