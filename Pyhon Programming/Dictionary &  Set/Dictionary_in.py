fde50697dbe825bdba6b28aade327a69032fdb6c
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-19 13:13:51 +0530

    Added New new program of Dictionary_in.py with alterations

--- Pyhon Programming/Dictionary &  Set/Dictionary_in.py
+++ Pyhon Programming/Dictionary &  Set/Dictionary_in.py
@@ -1,8 +1,14 @@
-Student = {
-    "name": 'John Doe',
+Student = {  #Creating a dictionary named Student
+    "name": 'John',
     "age": 20,
     "major": 'Computer Science',
     "CGPA": 3.5,
 }
 
+Student["surname"] = "Doe" # Adding a new key-value pair to the Student dictionary. The key is "surname" and the value is "Doe".
 print(Student)
+
+print(type(Student)) #This will print the type of the variable Student, which is <class 'dict'>, indicating that it is a dictionary.
+
+
+# Dictionaries are mutable, but we cannot have duplicate keys. If we try to add a key that already exists, it will overwrite the existing value.
