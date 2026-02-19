999155038c0310ebab7426a00c9e6920cbab71a8
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-19 12:42:06 +0530

    Added New prgram Question16 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question16.py
@@ -0,0 +1,8 @@
+# Store the above values in a list & sort them from "A" to "D". ["A", "B", "A", "C", "A", "D"]
+
+# Store the above values in a list
+grades = ["A", "B", "A", "C", "A", "D"] 
+# Sort the list from "A" to "D"
+grades.sort()
+print(grades) # Output: ['A', 'A', 'A', 'B', 'C', 'D']
+
