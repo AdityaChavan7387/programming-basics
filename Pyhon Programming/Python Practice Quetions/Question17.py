61bb508450856fa738ab0292e16a2db70b7a50eb
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-21 13:13:29 +0530

    Added New program that is in Question Practice folder Named Question17 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question17.py
@@ -0,0 +1,9 @@
+# Write a program to store the following word meanings in python dictionary :
+                                            # table : "A Piece of furniture","Lists of facts and figures"
+                                            # cat : "A Small Animal"
+                                            
+Word_Meanings = {
+    "table": ["A Piece of furniture", "Lists of facts and figures"],
+    "cat": "A Small Animal"
+}
+print(Word_Meanings)
