5026c7615d8f408d94d5393b1e02e15dcfba86f0
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-22 19:06:09 +0530

    Added New program that is in Question Practice folder Named Question20 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question20.py
@@ -0,0 +1,8 @@
+# Figure out a way to store 9 & 9.0 as separate values in the set.
+# (You can take help of built-in data types)
+
+# Using different data types to store them separately
+s = {9, "9.0"}   # 9 is integer, "9.0" is string
+
+print("Set values:")
+print(s)
