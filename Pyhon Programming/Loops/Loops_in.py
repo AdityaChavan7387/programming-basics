71273cedb4cee1c2d8b7f4d5db13253571b3c4c5
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-23 10:50:58 +0530

    Added New folder for loops in python with one simple program

---
+++ Pyhon Programming/Loops/Loops_in.py
@@ -0,0 +1,7 @@
+# Simple while loop.
+
+count = 1 # Initialize count to 1.
+
+while count <= 5: # Loop will run as long as count is less than or equal to 5.
+    print("Hello, World!")
+    count += 1 # Increment count by 1 to avoid infinite loop.
