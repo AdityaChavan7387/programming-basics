ea25352b4af80dcef6184a4d55a85bbf5ffd8573
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-23 11:38:21 +0530

    Added New program of how can we use continue in loops in python

---
+++ Pyhon Programming/Loops/Continue_in_loop.py
@@ -0,0 +1,9 @@
+
+i = 0
+
+while i <= 5:
+    if (i == 3):
+        i += 1
+        continue # it acts as a skip statement, it skips the current iteration and moves to the next one
+    print(i)
+    i += 1
