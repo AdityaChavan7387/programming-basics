3d4900bf77617b2ebe9f6788949d5fb82e041e53
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-23 11:02:33 +0530

    Added New program Named Question21 in python with altration

--- Pyhon Programming/Python Practice Quetions/Question21.py
+++ Pyhon Programming/Python Practice Quetions/Question21.py
@@ -1,7 +1,15 @@
 # Print numbers from 1 to 100 using a while loop.
 
-Number = 1 # Initialize Number to 1.
+# Number = 1 # Initialize Number to 1.
 
-while Number <= 100: # Loop will run as long as Number is less than or equal to 100.
+# while Number <= 100: # Loop will run as long as Number is less than or equal to 100.
+#     print(Number)
+#     Number += 1 # Increment Number by 1 to avoid infinite loop.
+
+
+# Print numbers from 100 to 1 using a while loop.
+Number = 100 # Initialize Number to 100.
+
+while Number >= 1: # Loop will run as long as Number is greater than or equal to 1.
     print(Number)
-    Number += 1 # Increment Number by 1 to avoid infinite loop.
+    Number -= 1 # Decrement Number by 1 to avoid infinite loop.
