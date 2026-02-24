969b8de959ecc9c4eb2e0affd4bfe9856a1a7a04
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-24 11:16:14 +0530

    Added New program of Quesion23 in python

---
+++ Pyhon Programming/Python Practice Quetions/Question23.py
@@ -0,0 +1,8 @@
+# print the elements of the list using a for loop
+
+# Sample data: List of fruits
+list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Print each element in the list using a for loop
+for element in list:
+    print(element)
