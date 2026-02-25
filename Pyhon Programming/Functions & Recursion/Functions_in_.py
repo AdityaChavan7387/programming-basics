9b953a0c6e827565e1ac79d8ab1900f2d51a4dce
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-25 12:26:12 +0530

    Added New Program on Functions & how it works in python

---
+++ Pyhon Programming/Functions & Recursion/Functions_in_.py
@@ -0,0 +1,14 @@
+a = 5
+b = 10
+
+sum = a + b  # type: ignore
+print(sum)
+
+# here we will learn about functions in python
+
+x = 5
+y = 10
+def sum(x, y):
+    return x + y
+result = sum(5, 10)
+print(result)
