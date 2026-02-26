f203ce1ffc7910b71e919c36747d189c5dd03e9a
Aditya Chavan <adityachavan7387@gmail.com>
2026-02-26 12:20:25 +0530

    Added New Program in python

---
+++ Pyhon Programming/File I-O in Python/Modules.py
@@ -0,0 +1,10 @@
+# Here we will see Modules in Python. Modules are files containing Python code that can be imported and used in other Python programs. They allow us to organize our code into reusable and manageable pieces.  
+
+import os  # This is a built-in module that provides functions for interacting with the operating system.
+import math  # This is a built-in module that provides mathematical functions and constants.
+
+# for Others Modules we have to install them using pip. For example, if we want to use the 'requests' module, we can install it using the command: pip install requests
+
+os.system('echo Hello, World!')  # This will execute the command 'echo Hello, World!' in the operating system's shell.
+
+# os.remove()  # This will remove the file named 'example.txt' from the current directory.
